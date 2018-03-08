6cf0372d9a2950d6e553479bdacbe554c8610566
Marta <mchupil@mail.ru>
2018-03-08 22:21:56 +0200

    some changes

--- Silvers/Controllers/MoviesController.cs
+++ Silvers/Controllers/MoviesController.cs
@@ -15,5 +15,16 @@ namespace Silvers.Controllers
             var movie = new Movie() { Name = "Shape of water" };
             return View(movie);
         }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }

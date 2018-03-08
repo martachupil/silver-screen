41d7485134fadef0288f7ab5c0d9a5a8e2ede2d2
Marta <mchupil@mail.ru>
2018-03-08 22:39:56 +0200

    added route

--- Silvers/App_Start/RouteConfig.cs
+++ Silvers/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Silvers
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Silvers/Controllers/MoviesController.cs
+++ Silvers/Controllers/MoviesController.cs
@@ -16,15 +16,10 @@ namespace Silvers.Controllers
             return View(movie);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
     }
 }
